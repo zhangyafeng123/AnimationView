ceedb9c4b0d8ef0d775137684167c07f50f357c0
zhangyafeng123 <1240334911@qq.com>
2017-11-14 09:53:06 +0800

    Initial Commit

---
+++ View的遮罩效果/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  View的遮罩效果
+//
+//  Created by linjianguo on 2017/11/14.
+//  Copyright © 2017年 com.justsee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
